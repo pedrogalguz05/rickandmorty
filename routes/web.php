a80e758dbed2237a9a8fdabc2d0b55cab02e24e6
Your Name <you@example.com>
2022-02-05 12:49:37 -0600

    API

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ApiController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('inicio',[ApiController::class, 'inicio'])->name('inicio');
+
+Route::get('personaje/{id}',[ApiController::class, 'detallePersonaje'])->name('detalle.personaje');
